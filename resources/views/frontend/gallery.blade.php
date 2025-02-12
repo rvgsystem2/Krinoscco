b36dfd348bd55a9ae700ed4f4846bdd09bfc3840
rvgsystem2 <rvgsystem2@gmail.com>
2025-02-12 12:44:26 +0530

    main

--- resources/views/frontend/gallery.blade.php
+++ resources/views/frontend/gallery.blade.php
@@ -100,7 +100,7 @@
                 <div>
                     <img src="{{ asset('asset/images/suite1.jpg') }}" alt="Modern Masterpiece"
                          class="w-full h-64 object-cover rounded-lg shadow-lg cursor-pointer"
-                         onclick="openModal('{{ asset('asset/images/suite1.jpg') }}')">
+                         onclick="openModal('{{ asset('asset/images/s1.jpg') }}')">
                     <div class="text-center text-lg font-semibold mt-2">Modern Masterpiece</div>
                 </div>
 
