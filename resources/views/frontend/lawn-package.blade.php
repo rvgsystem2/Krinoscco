13e68eec5b5ff3b856944377da1adbae285123fc
Kajal kumari <kajaldewan2023@gmail.com>
2025-03-24 12:00:17 +0530

    saaert

--- resources/views/frontend/lawn-package.blade.php
+++ resources/views/frontend/lawn-package.blade.php
@@ -37,7 +37,7 @@
 
         <div class="bg-white shadow-lg rounded-lg overflow-hidden">
             <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2">
-                <img src="{{ asset('asset/lawn/lawn-1.jpg') }}" class="w-full h-56 object-cover cursor-pointer"
+                <img src="{{ asset('asset/lawn/lawn-1.JPG') }}" class="w-full h-56 object-cover cursor-pointer"
                     onclick="openModal(this)">
                 <img src="{{ asset('asset/lawn/lawn-2.jpg') }}" class="w-full h-56 object-cover cursor-pointer"
                     onclick="openModal(this)">
